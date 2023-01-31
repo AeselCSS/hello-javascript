67df0678e970e1b8e916b5a265bf22bf4f8cd19e
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-01-31 15:45:35 +0100

    altered to work with form

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const sayHello = () => {
-    document.querySelector("#display").textContent =
-    "Hej " + document.querySelector("#name").value + 
-    ", din alder er " + document.querySelector("#age").value +
-    " og din email er " + document.querySelector("#email").value;
-}
+// const sayHello = () => {
+//     document.querySelector("#display").textContent =
+//     "Hej " + document.querySelector("#name").value + 
+//     ", din alder er " + document.querySelector("#age").value +
+//     " og din email er " + document.querySelector("#email").value;
+// }
 
-document.querySelector("#btn").addEventListener("click", sayHello);
+document.querySelector("#user-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.querySelector("#name").value;
+    const age = document.querySelector("#age").value;
+    const email = document.querySelector("#email").value;
+
+    document.querySelector("#display").textContent = `Hej ${name}, din alder er ${age} og din email er ${email}`;
+});
